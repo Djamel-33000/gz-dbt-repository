573bf33af9e567782d93b4533aed4e8544f8cb4a
phytotechnie2000@yahoo.fr <phytotechnie2000@yahoo.fr>
2026-02-05 11:51:29 +0000

    supprimer le doublons

--- models/staging/raw/stg_raw__vente.sql
+++ models/staging/raw/stg_raw__vente.sql
@@ -8,12 +8,15 @@ source as (
 
 renamed as (
 
-    select
-        date_date,
-        orders_id,
-        pdt_id,
-        revenue,
-        quantity
+    SELECT
+    -- On combine orders_id et pdt_id (et non product_id)
+    CONCAT(orders_id, '_', pdt_id) AS sales_id,
+    date_date,
+    orders_id,
+    pdt_id,
+    revenue,
+    quantity
+
 
     from source
 
