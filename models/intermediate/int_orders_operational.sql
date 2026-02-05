aae5780a5046d4f8f0b7a32081c12010b3713076
phytotechnie2000@yahoo.fr <phytotechnie2000@yahoo.fr>
2026-02-05 20:57:54 +0000

    finance day

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,20 +1,16 @@
 SELECT
     o.orders_id,
     o.date_date,
+    o.revenue,
     o.margin,
+    o.purchase_cost, -- <--- CETTE COLONNE MANQUAIT AUSSI
+    o.quantity,      -- <--- VÉRIFIE QUE CELLE-CI EST LÀ POUR FINANCE_DAYS
     s.shipping_fee,
     s.logcost,
     s.ship_cost,
-    -- Calcul sécurisé
-    ROUND(
-        CAST(o.margin AS FLOAT64) + 
-        CAST(s.shipping_fee AS FLOAT64) - 
-        (CAST(s.logcost AS FLOAT64) + CAST(s.ship_cost AS FLOAT64)), 
-        2
-    ) AS operational_margin
+    ROUND(CAST(o.margin AS FLOAT64) + s.shipping_fee - (s.logcost + s.ship_cost), 2) AS operational_margin
 FROM {{ ref('int_orders_margin') }} o
 LEFT JOIN {{ ref('stg_raw__livraison') }} s 
-    -- On force la comparaison entre deux STRING pour éviter l'erreur d'incompatibilité
     ON CAST(o.orders_id AS STRING) = CAST(s.orders_id AS STRING)
 
     
