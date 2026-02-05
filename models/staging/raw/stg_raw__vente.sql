75b62c0c104ec7c38b64bfdb116997de689e7658
phytotechnie2000@yahoo.fr <phytotechnie2000@yahoo.fr>
2026-02-05 12:59:04 +0000

    comit

--- models/staging/raw/stg_raw__vente.sql
+++ models/staging/raw/stg_raw__vente.sql
@@ -3,23 +3,18 @@ with
 source as (
 
     select * from {{ source('raw', 'vente') }}
-
 ),
 
 renamed as (
-
     SELECT
-    -- On combine orders_id et pdt_id (et non product_id)
-    CONCAT(orders_id, '_', pdt_id) AS sales_id,
-    date_date,
-    orders_id,
-    pdt_id,
-    revenue,
-    quantity
-
-
+        -- On utilise pdt_id (le nom réel dans ta source) pour créer la clé unique
+        CONCAT(orders_id, '_', pdt_id) AS sales_id,
+        date_date,
+        orders_id,
+        pdt_id,
+        revenue,
+        quantity
     from source
-
 )
 
 select * from renamed
